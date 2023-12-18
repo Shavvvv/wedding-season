51fa1195063f1f2f0a1a75683861f11ed5ae0876
Ed Gonzales <edgonzales6@gmail.com>
2023-12-18 07:12:11 -0800

    Initial Wedding Detail Functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+
+from .models import Wedding
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+class WeddingDetail(DetailView):
+    model = Wedding
