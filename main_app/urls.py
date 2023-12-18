3509ec0b90ab75b9c403f8fe796ddeb778d52661
mmarentette <melrosemarentette@gmail.com>
2023-12-18 11:25:47 -0500

    Build Wedding Delete functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,7 +5,8 @@ from . import views
 # Defined url routes
 urlpatterns = [
     path('', views.home, name='home'),
-    path('weddings/<int:pk>/', views.WeddingDetail.as_view(), name='weddings_detail'),
     path('weddings/', views.WeddingList.as_view(), name='weddings_list'),
+    path('weddings/<int:pk>/', views.WeddingDetail.as_view(), name='weddings_detail'),
     path('weddings/create/', views.WeddingCreate.as_view(), name='weddings_create'),
+    path('weddings/<int:pk>/delete', views.WeddingDelete.as_view(), name='weddings_delete'),
 ]
