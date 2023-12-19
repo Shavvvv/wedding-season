fe073f51b1300cbe7a1667d91761d54d48114d72
mmarentette <melrosemarentette@gmail.com>
2023-12-19 15:03:53 -0500

    Update signup function to display both User and Profile fields; create User and Profile objects on signup with one:one relationship

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,6 +1,6 @@
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
-from .models import Event, User
+from .models import Event, User, Profile
 
 
 class EventForm(ModelForm):
@@ -8,7 +8,12 @@ class EventForm(ModelForm):
         model = Event
         fields = ['type', 'description', 'start_date_time', 'end_date_time', 'venue']
 
-class UserProfileForm(UserCreationForm):
+class UserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+
+class ProfileForm(ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['type']
